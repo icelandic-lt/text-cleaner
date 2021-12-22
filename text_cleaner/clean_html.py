b97b2d0771e88e522e3d50e5af7ced2b6ecbb300
Kristober <kriztone1@gmail.com>
2021-12-22 12:09:28 +0000

    tidying up and adding an argument parser for command line availability

--- text_cleaner/clean_html.py
+++ text_cleaner/clean_html.py
@@ -1,6 +1,7 @@
 
+import argparse
 import re
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as beautiful_soup
 from text_cleaner import constants
 
 
@@ -13,14 +14,14 @@ def remove_whitespace_before_punctuation(text):
 def remove_duplicate_punctuation_marks(soup):
     # the following regex demarks a string with 1 punctuation 
     # mark followed by 1 or more punctuation marks
-    return re.sub(r'([,.?!])[,.?!]+', r'\1', soup)
+    return re.sub(r'([,.:;?!])[,.:;?!]+', r'\1', soup)
 
 
 # TODO: Incomplete.
 # Currently this method only works for tables with the same amount of rows & columns
 # Also the headers are currently appended to all rows (even those before), this will become
 # problematic if a table with that description is in the html document.
-def organize_text_in_table(soup):
+def clean_html_tables(soup):
     """
     Organizes text in tables by prepending a header for each cell in it's 
     column and finally removes the rows containing only headers.
@@ -53,43 +54,62 @@ def append_punctuation_to_tag_content(text, html_tag_to_punctuation_mark):
     return text
 
 
+def extract_html_from_file(html_doc, extract_from_div):
+    html_doc_content = open(html_doc)
+    soup = beautiful_soup(html_doc_content.read(), features='html.parser')
+    soup = soup.find("div", extract_from_div)
+
+    return soup
+
+
 def clean_html(
-    html_document,
+    html_doc,
     html_tag_to_punctuation_mark={},
-    div_class_name={}, 
+    content_parent_div={},
+    write_to_file='',
 ):
     """
     Preprocess html document input for text cleaning by parsing text content 
     specified by input, by removing and replacing html tags based on dictionary input.
     
     Args:
-        html_document           : html document to be cleaned
+        html_doc                : html document to extract from
         html_tag_replacement    : dictionary of html tags to be convert 
-        div_class_name          : the target div that contains all the content to be cleaned
+        write_to_file           : name of output file
+        content_parent_div      : the parent div of all the content to be cleaned
 
     Returns:
-        str: html tag stripped text from the input html document
+        str: the input html document stripped of html tags
     """
 
-    html_doc = open(html_document)
-    soup = bs(html_doc.read())
-    soup = soup.find("div", div_class_name)
 
     # WIP - Read description for method
-    # soup = organize_text_in_table(soup)
+    # soup = clean_html_tables(soup)
 
-    text = append_punctuation_to_tag_content(soup, html_tag_to_punctuation_mark)
-    text = soup.get_text()
+    html_soup = extract_html_from_file(html_doc, extract_from_div=content_parent_div)
+
+    html_soup = append_punctuation_to_tag_content(html_soup, html_tag_to_punctuation_mark)
+    text = html_soup.get_text()
+
+
 
     text = remove_whitespace_before_punctuation(text)
     text = remove_duplicate_punctuation_marks(text)
 
-    # convenient while developing this html cleaner
-    # f = open("audiobook.txt", "a")
-    # f.write(str(text))
-    # f.close()
+    if write_to_file:
+        f = open(write_to_file, "a")    
+        f.write(str(text))
+        f.close()
 
     return text
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=argparse.FileType('r'), help="html file to be extracted")
+    args = parser.parse_args()
+    
+    return args
 
 
 def main():
@@ -99,10 +119,11 @@ def main():
 
     print(
         clean_html(
-        html_document='audio2.html',
-        html_tag_to_punctuation_mark=dictionary,
-        div_class_name={"class": "content-text"},
-    ))
+            html_doc='html_snip.html',
+            html_tag_to_punctuation_mark=dictionary,
+            content_parent_div={"class": "content-text"},
+            write_to_file='cleaned_html_text.txt'
+        ))
 
 
 if __name__ == '__main__':
