ed690cb10b2c64ea2e73c427e14c6fbb98410c5b
Anna Nikulásdóttir <anna@grammatek.com>
2022-02-07 20:41:15 +0000

    add bs4 requirements, info

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-	name="text-cleaner", 
+	name='text-cleaner',
+	version='0.1.0',
+	description='A text cleaning tool for speech and text processing',
+	author='Grammatek ehf',
+	author_email='info@grammatek.com',
+	url='https://github.com/grammatek/text-cleaner',
 	packages=find_packages(),
-	install_requires=['setuptools'],
+	install_requires=[
+		'setuptools',
+		'bs4'
+	],
 	python_requires='>=3.5',
 	entry_points={
 			'console_scripts': [
 				'text_cleaner=text_cleaner.clean:main'
-							   ]
-				 }
+							]
+				}
 	)
